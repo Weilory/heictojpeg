d88717842a06473bafbaa111b1daefe31ce27ae0
Weilory <54391313+Weilory@users.noreply.github.com>
2021-04-26 13:25:44 +0800

    implement pyheif

--- heictojpg.py
+++ heictojpg.py
@@ -1,16 +1,35 @@
 import os
+import pyheif
+from PIL import Image
+
+
+def heic2jpg(path, npath):
+    heif_file = pyheif.read(path)
+    image = Image.frombytes(
+        heif_file.mode, 
+        heif_file.size, 
+        heif_file.data,
+        "raw",
+        heif_file.mode,
+        heif_file.stride,
+        )
+
+    image.save(npath, "JPEG")
+    print('created ', npath)
+
+
 
 def recursor(dirpath):
-	print(dirpath)
+	# print(dirpath)
 	with os.scandir(dirpath) as it:
 		for entry in it:
 			if entry.name.endswith(".HEIC") and entry.is_file():
 				# print(entry.name, entry.path)
 				npath = entry.path.split('.')
 				npath.pop()
-				npath.append('jpg')
+				npath.append('JPEG')
 				npath = '.'.join(npath)
-				os.rename(entry.path, npath)
+				heic2jpg(entry.path, npath)
 			else:
 				if not entry.is_file():
 					# directory
